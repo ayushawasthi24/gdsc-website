37bf473e8b3d200cf50771e0aa2b5cfea3d61258
Ayush <ayushawasthi2409@gmail.com>
2023-10-16 00:09:18 +0530

    changes

--- src/data/WebDevelopment.js
+++ src/data/WebDevelopment.js
@@ -1,60 +1,65 @@
-import gaurav from '../public/gaurav.jpg'
-import harsh from '../public/harsh.jpg'
-import deepak from '../public/deepak.png'
-import rishi from '../public/rishi.jpg'
-import siddharth from '../public/Siddharth.jpeg'
-import ishan from '../public/ishan.jpg'
-import pvsekhar from '../public/PVSekhar.jpg'
-import ishita from '../public/ishita1.jpg'
+import gaurav from "../public/gaurav.jpg";
+import harsh from "../public/harsh.jpg";
+import deepak from "../public/deepak.png";
+import rishi from "../public/rishi.jpg";
+import siddharth from "../public/Siddharth.jpeg";
+import ishan from "../public/ishan.jpg";
+import pvsekhar from "../public/PVSekhar.jpg";
+import ishita from "../public/ishita1.jpg";
+import ayush from "../public/ayush.jpeg";
 export const WebDevelopment = [
-    {
-        id: 1,
-        name: 'Gaurav Jain',
-        image_url:gaurav,
-        description : 'Web Development',       
-    },
-    {
-        id: 2,
-        name: 'Harsh Sinha',
-        image_url:harsh,
-        description : 'Web Development',       
-    },
-    {
-        id: 3,
-        name: 'Deepak Yadav',
-        image_url:deepak,
-        description : 'Web Development',       
-    },
-    {
-        id: 4,
-        name: 'Rishi Bharat Junghare',
-        image_url:rishi,
-        description : 'Web Development',       
-    },
-    {
-        id: 5,
-        name: 'Siddharth Agrawal',
-        image_url:siddharth,
-        description : 'Web Development',       
-    },
-    {
-        id: 6,
-        name: 'Ishan Porwal ',
-        image_url:ishan,
-        description : 'Web Development',       
-    },
-    {
-        id: 7,
-        name: 'P. V. Sekhar ',
-        image_url:pvsekhar,
-        description : 'Web Development',       
-    },
-    {
-        id:8,
-        name:'Ishita Pandey',
-        image_url:ishita,
-        description:'Web Development'
-    }
-
-    
-]
+  {
+    id: 1,
+    name: "Gaurav Jain",
+    image_url: gaurav,
+    description: "Web Development",
+  },
+  {
+    id: 2,
+    name: "Harsh Sinha",
+    image_url: harsh,
+    description: "Web Development",
+  },
+  {
+    id: 3,
+    name: "Deepak Yadav",
+    image_url: deepak,
+    description: "Web Development",
+  },
+  {
+    id: 4,
+    name: "Rishi Bharat Junghare",
+    image_url: rishi,
+    description: "Web Development",
+  },
+  {
+    id: 5,
+    name: "Siddharth Agrawal",
+    image_url: siddharth,
+    description: "Web Development",
+  },
+  {
+    id: 6,
+    name: "Ishan Porwal ",
+    image_url: ishan,
+    description: "Web Development",
+  },
+  {
+    id: 7,
+    name: "P. V. Sekhar ",
+    image_url: pvsekhar,
+    description: "Web Development",
+  },
+  {
+    id: 8,
+    name: "Ishita Pandey",
+    image_url: ishita,
+    description: "Web Development",
+  },
+  {
+    id: 9,
+    name: "Ayush Awasthi",
+    image_url: ayush,
+    description: "Web Development",
+  },
+];
